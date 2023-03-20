2687f44df9aa3d8b938b9dcb7bd7d571d7de8cf6
fishman123456 <den9152@yandex.ru>
2023-03-20 23:54:25 +0700

    Initial commit

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using Autodesk.AutoCAD.Runtime;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_DLL_Autocad_20_03_2023_
+{
+    public class Class1
+    {
+        [CommandMethod("WF_Form")]
+        public void Demo()
+        {
+            WPF_form er = new WPF_form();
+            er.Show();
+        }
+    }
+}
---
+++ WPF_form.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_DLL_Autocad_20_03_2023_
+{
+    /// <summary>
+    /// Логика взаимодействия для WPF_form.xaml
+    /// </summary>
+    public partial class WPF_form : Window
+    {
+        public WPF_form()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
